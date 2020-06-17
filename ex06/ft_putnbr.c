5dc83c84f0f814bccefc1f057be9b18767ef3f78
Piet Mahlangu <pimahlan@c4r6s1.wethinkcode.co.za>
2020-06-17 15:12:04 +0200

    day02

---
+++ ex06/ft_putnbr.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1,&c,1);
+}
+
+void	ft_putnbr(int nb)
+{	
+	char i;
+	int nb_copy;
+	int mod;
+
+	if (nb < 0)
+	{
+		nb *= -1;
+		ft_putchar('-');
+	}
+	nb_copy = nb;
+	if(nb_copy >=10)
+	{
+		mod = nb_copy % 10;
+	   	ft_putchar((nb_copy/10) + '0');
+		ft_putchar(mod + '0');
+	}
+	else
+		ft_putchar(nb + '0');
+   
+    ft_putchar('\n');
+}
+
+int main(void)
+{
+	ft_putnbr(422);
+	return(0);
+}
